83d18a9f8bdefc38e4b33817c7617de4ea9de0bb
ytbiu <174920143@qq.com>
2025-03-03 16:56:47 +0800

    minor

--- system/contracts/DRC20Factory/DRC20Factory.sol
+++ system/contracts/DRC20Factory/DRC20Factory.sol
@@ -4,10 +4,11 @@ pragma solidity ^0.8.18;
 import "./Token.sol";
 
 contract DRC20Factory {
+    event DRC20TokenDeployed(address indexed tokenAddress);
     constructor() {}
 
-    function deployDRC20Token(address _owner, string memory _name, string memory _symbol, uint256 supply) public returns (address) {
+    function deployDRC20Token(address _owner, string memory _name, string memory _symbol, uint256 supply) external {
         Token token = new Token(_owner,_name, _symbol, supply);
-        return address(token);
+        emit DRC20TokenDeployed(address(token));
     }
 }
